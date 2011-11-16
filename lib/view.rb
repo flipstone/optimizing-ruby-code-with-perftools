bd49363d1e2499aa711d532b385b0dbc586a824e
David Vollbracht <david.vollbracht@gmail.com>
2011-11-16 08:19:55 -0500

    Eager load organizations for investments

--- lib/view.rb
+++ lib/view.rb
@@ -2,7 +2,7 @@ class View
   VIEW_PATH = File.join File.dirname(__FILE__), 'view.html.haml'
 
   def self.render
-    @investments = Investment.order('cost DESC')
+    @investments = Investment.order('cost DESC').includes(:organization)
     @organizations = Organization.with_cached_relations
 
     ::Haml::Engine.new(File.read(VIEW_PATH), :filename => 'view.html.haml').render(self)
